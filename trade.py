93c407342387686e84fa0f8ce693303e7ac026b1
Eakkarat-Patt <73377720+Eakkarat-Patt@users.noreply.github.com>
2021-09-09 16:12:04 +0700

    initial commit

--- trade.py
+++ trade.py
@@ -1,5 +1,6 @@
 import requests
+from config import *
 
 BASE_URL = 'https://paper-api.alpaca.markets'
 
-r = requests.get(BASE_URL, )
+r = requests.get(BASE_URL, headers={'APCA-API-KEY-ID':API_KEY, 'APCA-API-SECRET-KEY': SECRET_KEY})
